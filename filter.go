9be653e9f4ac28b4631c5b144246137e83369e11
may <mehrabhoque@gmail.com>
2019-01-07 17:25:18 -0500

    Add documentation comments to filter.go

--- filter.go
+++ filter.go
@@ -2,16 +2,27 @@ package jasper
 
 import "github.com/pkg/errors"
 
+// Filter is type for classifying and grouping types of processes in filter
+// operations, such as that found in List() on Managers.
 type Filter string
 
 const (
-	Running    Filter = "running"
-	Terminated        = "terminated"
-	All               = "all"
-	Failed            = "failed"
-	Successful        = "successful"
+	// Running is a filter for processes that have not yet completed and are
+	// still running.
+	Running Filter = "running"
+	// Terminated is the opposite of the Running filter.
+	Terminated = "terminated"
+	// All is a filter that is satisfied by any process.
+	All = "all"
+	// Failed refers to processes that have terminated unsuccessfully.
+	Failed = "failed"
+	// Successful refers to processes that have terminated successfully.
+	Successful = "successful"
 )
 
+// Validate ensures that the Filter on which it is called is a valid, supported
+// Filter value.
+// Valid filter types are all documented.
 func (f Filter) Validate() error {
 	switch f {
 	case Running, Terminated, All, Failed, Successful:
