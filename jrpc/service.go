2b0f9e22bf296e5d571a0ef61ab77201b97c72d9
may <mehrabhoque@gmail.com>
2019-01-07 18:08:30 -0500

    Add documentation comments to jrpc/service.go

--- jrpc/service.go
+++ jrpc/service.go
@@ -7,6 +7,9 @@ import (
 	grpc "google.golang.org/grpc"
 )
 
+// AttachService attaches the given manager to the jasper GRPC server. After
+// this function successfully returns, calls to Manager functions will be sent
+// over GRPC to the Jasper GRPC server.
 func AttachService(manager jasper.Manager, s *grpc.Server) error {
 	return errors.WithStack(internal.AttachService(manager, s))
 }
