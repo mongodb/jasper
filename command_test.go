0aba5dcb801fc975a5c77ac036541e55b7d96426
may <mehrabhoque@gmail.com>
2019-01-17 11:45:11 -0500

    Clean up command_test.go, add ignoreOnError test

--- command_test.go
+++ command_test.go
@@ -4,12 +4,18 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 
-	"github.com/mongodb/grip"
 	"github.com/mongodb/grip/level"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	echo, ls         = "echo", "ls"
+	arg1, arg2, arg3 = "123", "456", "789"
 )
 
 type BufferCloser struct {
@@ -18,17 +24,37 @@ type BufferCloser struct {
 
 func (b *BufferCloser) Close() error { return nil }
 
+func verifyOutput(cmd *Command, t *testing.T, expectSuccess bool, expectedOutputs ...string) {
+	var buf bytes.Buffer
+	bufCloser := &BufferCloser{&buf}
+
+	cmd.SetOutputWriter(bufCloser)
+	if expectSuccess {
+		assert.NoError(t, cmd.Run(context.Background()))
+	} else {
+		assert.Error(t, cmd.Run(context.Background()))
+	}
+
+	output := bufCloser.String()
+
+	for _, expected := range expectedOutputs {
+		assert.True(t, strings.Contains(output, expected))
+	}
+}
+
 func TestCommandImplementation(t *testing.T) {
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
 	for name, testCase := range map[string]func(context.Context, *testing.T){
 		"ValidRunCommandDoesNotError": func(ctx context.Context, t *testing.T) {
 			assert.NoError(
 				t,
 				RunCommand(
 					ctx,
-					"test",
+					t.Name(),
 					level.Info,
-					[]string{"echo", "hello world"},
-					"/Users/may/quick/",
+					[]string{echo, arg1},
+					cwd,
 					map[string]string{},
 				),
 			)
@@ -38,44 +64,55 @@ func TestCommandImplementation(t *testing.T) {
 				t,
 				RunCommand(
 					ctx,
-					"test",
+					t.Name(),
 					level.Info,
-					[]string{"ls", "DNE"},
-					"/Users/may/quick/",
+					[]string{ls, arg2},
+					cwd,
 					map[string]string{},
 				),
 			)
 		},
+		"SingleInvalidSubCommandCausesTotalError": func(ctx context.Context, t *testing.T) {
+			assert.Error(
+				t,
+				RunCommandGroup(
+					ctx,
+					t.Name(),
+					level.Info,
+					[][]string{
+						[]string{echo, arg1},
+						[]string{ls, arg2},
+						[]string{echo, arg3},
+					},
+					cwd,
+					map[string]string{},
+				),
+			)
+		},
+		"IgnoreErrorCausesSuccessfulReturn": func(ctx context.Context, t *testing.T) {
+			cmd := NewCommand()
+			cmd.Extend([][]string{
+				[]string{echo, arg1},
+				[]string{ls, arg3},
+				[]string{echo, arg2},
+			})
+			cmd.SetIgnoreError(true)
+			verifyOutput(cmd, t, true, arg1, fmt.Sprintf("%s: %s: No such file or directory", ls, arg3), arg2)
+		},
 		"CommandOutput": func(ctx context.Context, t *testing.T) {
-			echo, ls := "echo", "ls"
-			arg1, arg2, arg3 := "lalala", "second", "DNE"
-
-			verifyOutput := func(cmd *Command, t *testing.T, expectedOutputs ...string) {
-				var buf bytes.Buffer
-				bufCloser := &BufferCloser{&buf}
-
-				cmd.SetOutputWriter(bufCloser)
-				assert.NoError(t, cmd.Run(context.Background()))
-				output := bufCloser.String()
-				grip.Debugf("Got output: %v", output)
-
-				for _, expected := range expectedOutputs {
-					assert.True(t, strings.Contains(output, expected))
-				}
-			}
 
 			for subName, subTestCase := range map[string]func(context.Context, *testing.T){
 				"StdOutOnly": func(ctx context.Context, t *testing.T) {
 					cmd := NewCommand()
 					cmd.Add([]string{echo, arg1})
 					cmd.Add([]string{echo, arg2})
-					verifyOutput(cmd, t, arg1, arg2)
+					verifyOutput(cmd, t, true, arg1, arg2)
 				},
 				"StdErrOnly": func(ctx context.Context, t *testing.T) {
 					cmd := NewCommand()
 					cmd.Add([]string{ls, arg3})
 					lsOutput := fmt.Sprintf("%s: %s: No such file or directory", ls, arg3)
-					verifyOutput(cmd, t, lsOutput)
+					verifyOutput(cmd, t, false, lsOutput)
 				},
 				"StdOutAndStdErr": func(ctx context.Context, t *testing.T) {
 					cmd := NewCommand()
@@ -83,7 +120,7 @@ func TestCommandImplementation(t *testing.T) {
 					cmd.Add([]string{echo, arg2})
 					cmd.Add([]string{ls, arg3})
 					lsOutput := fmt.Sprintf("%s: %s: No such file or directory", ls, arg3)
-					verifyOutput(cmd, t, arg1, arg2, lsOutput)
+					verifyOutput(cmd, t, false, arg1, arg2, lsOutput)
 				},
 			} {
 				t.Run(subName, func(t *testing.T) {
