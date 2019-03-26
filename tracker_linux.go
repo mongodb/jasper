ea898fb90a5a111b77034c8263f9a60b1bd5bf3e
Kim Tao <kim.tao@mongodb.com>
2019-03-26 17:07:06 -0400

    Begin writing containerd

--- tracker_linux.go
+++ tracker_linux.go
@@ -1,17 +1,52 @@
 package jasper
 
-// TODO: implement
+import (
+	"fmt"
 
-type linuxProcessTracker struct{}
+	"github.com/containerd/cgroups"
+	"github.com/pkg/errors"
+)
+
+// TODO: implement
+// kim: TODO: allow cgroups to:
+// 1. add procs to the list (for later killing).
+// 2. impose limits (via a separate functionality).
+
+type linuxProcessTracker struct {
+	cgroups map[string]cgroups.Cgroup
+}
 
 func newProcessTracker(name string) (processTracker, error) {
-	return &linuxProcessTracker{}, nil
+	shares := uint64(100)
+	cgroups := make(map[string]cgroups.Cgroup)
+
+	cgroup, err := cgroups.New(cgroups.V1, cgroups.StaticPath("/jasper"), &specs.LinuxResources{
+		CPU: &specs.CPU{
+			Shares: &shares,
+		},
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create cgroup")
+	}
+	fmt.Println(cgroup)
+
+	return &linuxProcessTracker{cgroups: cgroups}, nil
 }
 
-func (_ *linuxProcessTracker) add(pid uint) error {
+func (t *linuxProcessTracker) add(pid uint) error {
+	proc := cgroups.Process{}
+	t.cgroup.Add(proc)
 	return nil
 }
 
-func (_ *linuxProcessTracker) cleanup() error {
-	return nil
+// kim: TODO: addLimit for other OSes
+// func (t *linuxProcessTracker) addLimit(pid uint, limit string) error {
+//     return nil
+// }
+
+func (t *linuxProcessTracker) cleanup() error {
+	// Get all procs.
+	// t.cgroup.Processes("something", false)
+	// Send SIGTERM to all procs.
+	// for _, proc := range procs
 }
