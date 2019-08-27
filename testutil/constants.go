4bab77c2947f8357dc53f46757f37ac41c6d186e
tycho garen <garen@tychoish.com>
2019-08-26 21:07:40 -0400

    add rpc task timeout

--- testutil/constants.go
+++ testutil/constants.go
@@ -4,6 +4,7 @@ import "time"
 
 const (
 	TaskTimeout        = 5 * time.Second
+	RPCTaskTimeout     = 30 * time.Second
 	ProcessTestTimeout = 15 * time.Second
 	ManagerTestTimeout = 5 * TaskTimeout
 	LongTaskTimeout    = 100 * time.Second
