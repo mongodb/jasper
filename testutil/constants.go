fb97efad452e3a884ee948243851e3179ab6e49a
Sam Kleinman <sam@10gen.com>
2019-08-27 12:33:25 -0400

    fix spelling

--- testutil/constants.go
+++ testutil/constants.go
@@ -3,9 +3,9 @@ package testutil
 import "time"
 
 const (
-	TestTimeoput       = 5 * time.Second
+	TestTimeout        = 5 * time.Second
 	RPCTestTimeout     = 30 * time.Second
 	ProcessTestTimeout = 15 * time.Second
-	ManagerTestTimeout = 5 * TestTimeoput
+	ManagerTestTimeout = 5 * TestTimeout
 	LongTestTimeout    = 100 * time.Second
 )
