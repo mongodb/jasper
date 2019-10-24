495eeea1669d786f719d5608a6becef320666789
Kim Tao <kim.tao@mongodb.com>
2019-10-24 09:38:22 -0400

    Write proxy remote config

--- options/remote.go
+++ options/remote.go
@@ -10,7 +10,7 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-type config struct {
+type remoteConfig struct {
 	Host          string
 	User          string
 	Port          int
@@ -23,22 +23,19 @@ type config struct {
 }
 
 // Remote represents options to SSH into a remote machine.
-// kim: TODO: support proxy config
+// kim: TODO: support proxy remoteConfig
 type Remote struct {
-	config
-	// kim: TODO: this should be renamed
-	Proxy Proxy
+	remoteConfig
+	Proxy *Proxy
 }
 
 type Proxy struct {
-	config
+	remoteConfig
 }
 
 const defaultSSHPort = 22
 
-// Validate ensures that enough information is provided to connect to a remote
-// host.
-func (opts *Remote) Validate() error {
+func (opts *remoteConfig) validate() error {
 	catcher := grip.NewBasicCatcher()
 	if opts.Host == "" {
 		catcher.New("host cannot be empty")
@@ -61,6 +58,64 @@ func (opts *Remote) Validate() error {
 	return catcher.Resolve()
 }
 
+func (opts *remoteConfig) resolve() (*ssh.ClientConfig, error) {
+	var auth []ssh.AuthMethod
+	if opts.Key != "" || opts.KeyFile != "" {
+		pubkey, err := opts.publicKeyAuth()
+		if err != nil {
+			return nil, errors.Wrap(err, "coudl not get public key")
+		}
+		auth = append(auth, pubkey)
+	}
+	if opts.Password != "" {
+		auth = append(auth, ssh.Password(opts.Password))
+	}
+	return &ssh.ClientConfig{
+		Timeout:         opts.Timeout,
+		User:            opts.User,
+		Auth:            auth,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}, nil
+}
+
+func (opts *remoteConfig) publicKeyAuth() (ssh.AuthMethod, error) {
+	var key []byte
+	if opts.KeyFile != "" {
+		var err error
+		key, err = ioutil.ReadFile(opts.KeyFile)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not read key file")
+		}
+	} else {
+		key = []byte(opts.Key)
+	}
+
+	var signer ssh.Signer
+	var err error
+	if opts.KeyPassphrase != "" {
+		signer, err = ssh.ParsePrivateKeyWithPassphrase(key, []byte(opts.KeyPassphrase))
+	} else {
+		signer, err = ssh.ParsePrivateKey(key)
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get signer")
+	}
+	return ssh.PublicKeys(signer), nil
+}
+
+// Validate ensures that enough information is provided to connect to a remote
+// host.
+func (opts *Remote) Validate() error {
+	catcher := grip.NewBasicCatcher()
+
+	if opts.Proxy != nil {
+		catcher.Wrap(opts.Proxy.validate(), "invalid proxy config")
+	}
+
+	catcher.Add(opts.validate())
+	return catcher.Resolve()
+}
+
 func (opts *Remote) String() string {
 	if opts.User == "" {
 		return opts.Host
@@ -74,26 +129,51 @@ func (opts *Remote) Resolve() (*ssh.Client, *ssh.Session, error) {
 		return nil, nil, errors.Wrap(err, "invalid remote options")
 	}
 
-	var auth []ssh.AuthMethod
-	if opts.KeyFile != "" {
-		pubkey, err := opts.publicKey()
+	var client *ssh.Client
+	if opts.Proxy != nil {
+		proxyConfig, err := opts.Proxy.resolve()
 		if err != nil {
-			return nil, nil, errors.Wrap(err, "problem getting public key from file")
+			return nil, nil, errors.Wrap(err, "could not create proxy config")
 		}
-		auth = append(auth, pubkey)
-	}
-	if opts.Password != "" {
-		auth = append(auth, ssh.Password(opts.Password))
-	}
-	config := &ssh.ClientConfig{
-		Timeout:         opts.Timeout,
-		User:            opts.User,
-		Auth:            auth,
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}
-	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", opts.Host, opts.Port), config)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "problem dialing host")
+		proxyClient, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", opts.Proxy.Host, opts.Proxy.Port), proxyConfig)
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "could not dial proxy")
+		}
+
+		targetConn, err := proxyClient.Dial("tcp", fmt.Sprintf("%s:%d", opts.Host, opts.Port))
+		if err != nil {
+			catcher := grip.NewBasicCatcher()
+			catcher.Wrap(proxyClient.Close(), "error closing connection to proxy")
+			catcher.Wrap(err, "could not dial target host")
+			return nil, nil, catcher.Resolve()
+		}
+
+		targetConfig, err := opts.resolve()
+		if err != nil {
+			catcher := grip.NewBasicCatcher()
+			catcher.Wrap(proxyClient.Close(), "error closing connection to proxy")
+			catcher.Wrap(err, "could not create target config")
+			return nil, nil, catcher.Resolve()
+		}
+		gatewayConn, chans, reqs, err := ssh.NewClientConn(targetConn, fmt.Sprintf("%s:%d", opts.Host, opts.Port), targetConfig)
+		if err != nil {
+			catcher := grip.NewBasicCatcher()
+			catcher.Wrap(targetConn.Close(), "error closing connection to target")
+			catcher.Wrap(proxyClient.Close(), "error closing connection to proxy")
+			catcher.Wrap(err, "could not establish connection to target via proxy")
+			return nil, nil, catcher.Resolve()
+		}
+		client = ssh.NewClient(gatewayConn, chans, reqs)
+	} else {
+		var err error
+		config, err := opts.resolve()
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "could not create config")
+		}
+		client, err = ssh.Dial("tcp", fmt.Sprintf("%s:%d", opts.Host, opts.Port), config)
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "could not dial host")
+		}
 	}
 
 	session, err := client.NewSession()
@@ -104,25 +184,4 @@ func (opts *Remote) Resolve() (*ssh.Client, *ssh.Session, error) {
 		return nil, nil, errors.Wrap(catcher.Resolve(), "could not establish session")
 	}
 	return client, session, nil
-}
-
-func (opts *Remote) publicKey() (ssh.AuthMethod, error) {
-	key, err := ioutil.ReadFile(opts.KeyFile)
-	if err != nil {
-		return nil, err
-	}
-
-	var signer ssh.Signer
-	if opts.KeyPassphrase != "" {
-		signer, err = ssh.ParsePrivateKeyWithPassphrase(key, []byte(opts.KeyPassphrase))
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		signer, err = ssh.ParsePrivateKey(key)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return ssh.PublicKeys(signer), nil
 }
