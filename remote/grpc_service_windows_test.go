e7bc8085a43916b6eeedae3d92377737e8478099
Sam Kleinman <sam@10gen.com>
2019-12-10 14:53:46 -0500

    fix windows tests

--- remote/grpc_service_windows_test.go
+++ remote/grpc_service_windows_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 
 	"github.com/mongodb/jasper"
-	"github.com/mongodb/jasper/rpc/internal"
+	"github.com/mongodb/jasper/remote/internal"
 	"github.com/mongodb/jasper/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
