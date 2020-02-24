76c7c532324b1c8cfdee3892dc27575fb8f91de3
Sam Kleinman <sam@10gen.com>
2020-02-24 18:36:13 -0500

    loggg

--- remote/logging.go
+++ remote/logging.go
@@ -2,6 +2,7 @@ package remote
 
 import (
 	"github.com/mongodb/grip/level"
+	"github.com/mongodb/grip/message"
 	"github.com/mongodb/grip/send"
 	"github.com/mongodb/jasper"
 	"github.com/pkg/errors"
@@ -9,10 +10,10 @@ import (
 
 // LoggingPayload captures the arguements to the SendMessages operation.
 type LoggingPayload struct {
-	LoggerID string `bson:"logger_id" json:"logger_id" yaml:"logger_id"`
-
-	Messages         []interface{}        `bson:"messages" json:"messages" yaml:"messages"`
+	LoggerID         string               `bson:"logger_id" json:"logger_id" yaml:"logger_id"`
+	Data             interface{}          `bson:"data" json:"data" yaml:"data"`
 	Priority         level.Priority       `bson:"priority" json:"priority" yaml:"priority"`
+	IsMulti          bool                 `bson:"multi" json:"multi" yaml:"multi"`
 	ForceSendToError bool                 `bson:"force_send_to_error" json:"force_send_to_error" yaml:"force_send_to_error"`
 	AddMetadata      bool                 `bson:"add_metadata" json:"add_metadata" yaml:"add_metadata"`
 	Format           LoggingPayloadFormat `bson:"payload_format,omitempty" json:"payload_format,omitempty" yaml:"payload_format,omitempty"`
@@ -21,10 +22,8 @@ type LoggingPayload struct {
 type LoggingPayloadFormat string
 
 const (
-	LoggingPayloadFormatString      = "string"
-	LoggingPayloadFormatStringLines = "lines"
-	LoggingPayloadFormatBSON        = "bson"
-	LoggingPayloadFormatJSON        = "json"
+	LoggingPayloadFormatBSON = "bson"
+	LoggingPayloadFormatJSON = "json"
 )
 
 func (lp *LoggingPayload) Send(logger *jasper.CachedLogger) error {
@@ -44,8 +43,41 @@ func (lp *LoggingPayload) Send(logger *jasper.CachedLogger) error {
 		return errors.New("could not configure output for message")
 	}
 
-	switch lp.Format {
+	var msg message.Composer
 
+	switch data := lp.Data.(type) {
+	case string:
+		if lp.ForceSendToError == LoggingPayloadFormatJSON {
+
+		} else {
+			msg = message.NewDefaultMessage(lp.Priority, data)
+		}
+	case []byte:
+		switch lp.Format {
+		case LoggingPayloadFormatJSON:
+		case LoggingPayloadFormatBSON:
+		default:
+		}
+	case []string:
+		if lp.IsMulti {
+			varbatch := []message.Composer{}
+			for _, str := range data {
+				if lp.Format == LoggingPayloadFormatJSON {
+
+				} else {
+					batch = append(message.NewDefaultMessage(lp.Priority, str))
+				}
+			}
+
+			msg = message.NewGroupComposer(batch)
+		} else {
+
+		}
+
+	case [][]string:
+	case [][]byte:
+	case [][]interface{}:
 	}
 
+	sender.Send(msg)
 }
