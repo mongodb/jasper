382ef55443552b420c54c8e30a3fe243e6b7d7a9
Kim Tao <kim.tao@mongodb.com>
2020-08-20 16:46:16 -0400

    Don't export underlying buffer

--- util/buffer.go
+++ util/buffer.go
@@ -6,11 +6,11 @@ import (
 )
 
 // NewLocalBuffer returns a thread-safe Read/Write closer.
-func NewLocalBuffer(b bytes.Buffer) *LocalBuffer { return &LocalBuffer{Buffer: b} }
+func NewLocalBuffer(b bytes.Buffer) *LocalBuffer { return &LocalBuffer{buffer: b} }
 
 // LocalBuffer provides a thread-safe in-memory buffer.
 type LocalBuffer struct {
-	Buffer bytes.Buffer
+	buffer bytes.Buffer
 	sync.RWMutex
 }
 
@@ -18,14 +18,14 @@ type LocalBuffer struct {
 func (b *LocalBuffer) Read(p []byte) (n int, err error) {
 	b.RLock()
 	defer b.RUnlock()
-	return b.Buffer.Read(p)
+	return b.buffer.Read(p)
 }
 
 // Write performs a thread-safe in-memory write.
 func (b *LocalBuffer) Write(p []byte) (n int, err error) {
 	b.Lock()
 	defer b.Unlock()
-	return b.Buffer.Write(p)
+	return b.buffer.Write(p)
 }
 
 // String returns the in-memory buffer contents as a string in a thread-safe
@@ -33,7 +33,7 @@ func (b *LocalBuffer) Write(p []byte) (n int, err error) {
 func (b *LocalBuffer) String() string {
 	b.RLock()
 	defer b.RUnlock()
-	return b.Buffer.String()
+	return b.buffer.String()
 }
 
 // Close is a no-op to satisfy the closer interface..
