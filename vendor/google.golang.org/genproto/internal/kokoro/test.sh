9d95795c2fa69ae10b472b8fbf369373090a4b47
Kim Tao <kim.tao@mongodb.com>
2020-09-09 17:19:58 -0400

    Revendor dependencies to use previous Jasper version's pinned dependency versions

--- vendor/google.golang.org/genproto/internal/kokoro/test.sh
+++ vendor/google.golang.org/genproto/internal/kokoro/test.sh
@@ -15,19 +15,17 @@ go version
 export GOPATH="$HOME/go"
 export GENPROTO_HOME=$GOPATH/src/google.golang.org/genproto
 export PATH="$GOPATH/bin:$PATH"
-export GO111MODULE=on
 mkdir -p $GENPROTO_HOME
-
 
 # Move code into $GOPATH and get dependencies
 git clone . $GENPROTO_HOME
 cd $GENPROTO_HOME
 
 try3() { eval "$*" || eval "$*" || eval "$*"; }
+try3 go get -v -t ./...
 
-# All packages, including +build tools, are fetched.
-try3 go mod download
 ./internal/kokoro/vet.sh
+./internal/kokoro/check_incompat_changes.sh
 
 # Run tests and tee output to log file, to be pushed to GCS as artifact.
 go test -race -v ./... 2>&1 | tee $KOKORO_ARTIFACTS_DIR/$KOKORO_GERRIT_CHANGE_NUMBER.txt
--- vendor/google.golang.org/genproto/internal/kokoro/vet.sh
+++ vendor/google.golang.org/genproto/internal/kokoro/vet.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
 # Fail on any error
-set -eo
+set -eo pipefail
 
 # Display commands being run
 set -x
 
-# Only run the linter on go1.13, since it needs type aliases (and we only care
+# Only run the linter on go1.11, since it needs type aliases (and we only care
 # about its output once).
-if [[ `go version` != *"go1.13"* ]]; then
+# TODO(deklerk) We should pass an environment variable from kokoro to decide
+# this logic instead.
+if [[ `go version` != *"go1.11"* ]]; then
     exit 0
 fi
 
-go install \
-  github.com/golang/protobuf/protoc-gen-go \
-  golang.org/x/tools/cmd/goimports
+pwd
 
 # Fail if a dependency was added without the necessary go.mod/go.sum change
 # being part of the commit.
-go mod tidy
+GO111MODULE=on go mod tidy
 git diff go.mod | tee /dev/stderr | (! read)
 git diff go.sum | tee /dev/stderr | (! read)
 
-# Easier to debug CI.
-pwd
+try3() { eval "$*" || eval "$*" || eval "$*"; }
+
+try3 go get -u \
+  golang.org/x/lint/golint \
+  golang.org/x/tools/cmd/goimports \
+  honnef.co/go/tools/cmd/staticcheck
 
 # Look at all .go files (ignoring .pb.go files) and make sure they have a Copyright. Fail if any don't.
-find . -type f -name "*.go" ! -name "*.pb.go" -exec grep -L "\(Copyright [0-9]\{4,\}\)" {} \; 2>&1 | tee /dev/stderr | (! read)
-
+git ls-files "*[^.pb].go" | xargs grep -L "\(Copyright [0-9]\{4,\}\)" 2>&1 | tee /dev/stderr | (! read)
 gofmt -s -d -l . 2>&1 | tee /dev/stderr | (! read)
 goimports -l . 2>&1 | tee /dev/stderr | (! read)
 
