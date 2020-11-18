01469c7f659204217cec7b5425798a7a4a64be8d
julianedwards <julian.edwards@mongodb.com>
2020-11-18 12:48:17 -0500

    fix lint

--- options/test_util.go
+++ options/test_util.go
@@ -8,9 +8,9 @@ import (
 	"github.com/mongodb/grip/send"
 )
 
-// TODO: Remove this mock sender with EVG-13443.
 // MockSender is a simple mock implementation of the grip/send.Sender
 // interface.
+// TODO: Remove this mock sender with EVG-13443.
 type MockSender struct {
 	*send.Base
 	Closed bool
@@ -23,8 +23,13 @@ func NewMockSender(name string) *MockSender {
 	}
 }
 
-func (*MockSender) Send(_ message.Composer)       {}
+// Send noops.
+func (*MockSender) Send(_ message.Composer) {}
+
+// Flush noops.
 func (*MockSender) Flush(_ context.Context) error { return nil }
+
+// Close will fail if Closed is already set to false.
 func (s *MockSender) Close() error {
 	if s.Closed {
 		return errors.New("mock sender already closed")
